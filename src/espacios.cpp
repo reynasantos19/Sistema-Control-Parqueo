d6b89a9237d4e82471b2a274493da073eb56a7c1
Reyna <rsantosg1@miumg.edu.gt>
2026-09-17 20:11:22 -0600

    Agregar modulo de espacios

---
+++ src/espacios.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// Muestra los espacios disponibles del parqueo
+void mostrarDisponibilidad() {
+    cout << "===== DISPONIBILIDAD DE ESPACIOS =====" << endl;
+    cout << "Espacios disponibles: 10" << endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void mostrarDisponibilidad();
+
 int main() {
     cout << "====================================" << endl;
     cout << "   SISTEMA DE CONTROL DE PARQUEO" << endl;
     cout << "====================================" << endl;
     cout << "Sistema iniciado correctamente." << endl;
+
+    mostrarDisponibilidad();
 
     return 0;
 }
