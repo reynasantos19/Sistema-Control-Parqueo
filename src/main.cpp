aac1079b8a2edca39d94db59efa4870a5da60425
Reyna <rsantosg1@miumg.edu.gt>
2026-09-17 19:48:16 -0600

    Agregar programa inicial en C++

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "====================================" << endl;
+    cout << "   SISTEMA DE CONTROL DE PARQUEO" << endl;
+    cout << "====================================" << endl;
+    cout << "Sistema iniciado correctamente." << endl;
+
+    return 0;
+}
