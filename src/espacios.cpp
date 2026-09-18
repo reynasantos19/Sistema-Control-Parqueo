7d32ba9c5411d6a8579b84291e0d733796a89cff
Reyna <rsantosg1@miumg.edu.gt>
2026-09-17 20:17:51 -0600

    Actualizar parqueo a 50 espacios

--- src/espacios.cpp
+++ src/espacios.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-// Muestra los espacios disponibles del parqueo
+// Cantidad total de espacios del parqueo
+const int TOTAL_ESPACIOS = 50;
+
+// Estado de los espacios:
+// 0 = libre
+// 1 = ocupado
+int espacios[TOTAL_ESPACIOS] = {0};
+
+// Muestra el estado de cada espacio
 void mostrarDisponibilidad() {
     cout << "===== DISPONIBILIDAD DE ESPACIOS =====" << endl;
-    cout << "Espacios disponibles: 10" << endl;
+
+    for (int i = 0; i < TOTAL_ESPACIOS; i++) {
+        cout << "Espacio " << i + 1 << ": ";
+
+        if (espacios[i] == 0) {
+            cout << "Libre";
+        } else {
+            cout << "Ocupado";
+        }
+
+        cout << endl;
+    }
 }
